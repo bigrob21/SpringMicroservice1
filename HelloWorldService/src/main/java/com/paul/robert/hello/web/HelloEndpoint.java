95ff63a40414d241d0f1066af75ea60235991880
bigrob21 <bigrob21@homtail.com>
2018-03-31 22:57:33 -0400

    Adding an endpoint to the Helloworld Service. Add the Zuul Gateway Proxy Service to act as the Edge Service. Loaded PKI infrastructure to the Edge Service.

--- HelloWorldService/src/main/java/com/paul/robert/hello/web/HelloEndpoint.java
+++ HelloWorldService/src/main/java/com/paul/robert/hello/web/HelloEndpoint.java
@@ -1,8 +1,15 @@
 package com.paul.robert.hello.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloEndpoint {
 
+	@GetMapping(path="/")
+	public ResponseEntity<String> hello(){
+		return ResponseEntity.ok("Hello There!!!");
+	}
+	
 }
