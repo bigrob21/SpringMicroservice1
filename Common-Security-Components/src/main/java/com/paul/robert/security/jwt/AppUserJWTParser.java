a3ef242b03fd367cf05571fccea41b655c34a3cc
bigrob21 <bigrob21@homtail.com>
2018-04-03 07:30:46 -0400

    adding jackson databinding.

--- Common-Security-Components/src/main/java/com/paul/robert/security/jwt/AppUserJWTParser.java
+++ Common-Security-Components/src/main/java/com/paul/robert/security/jwt/AppUserJWTParser.java
@@ -4,19 +4,36 @@ import java.util.Calendar;
 import java.util.Optional;
 import java.util.UUID;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paul.robert.dto.security.model.AppUser;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 public class AppUserJWTParser implements JWTPrincipalParser<AppUser> {
 
 	private String secretKey;
+	@Getter @Setter
+	private long tokenExpirationInMillis = 30000;
+	@Getter @Setter
+	private ObjectMapper mapper = new ObjectMapper(); 
+	
 	
 	public AppUserJWTParser(String secret){
 		this.secretKey = Optional.ofNullable(secret)
 			.filter(str -> str != null && !str.trim().equals(""))
 			.orElseThrow(() -> new IllegalArgumentException("A secret key must be provided!"));
+	}
+	
+	public AppUserJWTParser(String secret, long tokenExpirationTime){
+		this.secretKey = Optional.ofNullable(secret)
+			.filter(str -> str != null && !str.trim().equals(""))
+			.orElseThrow(() -> new IllegalArgumentException("A secret key must be provided!"));
+		this.setTokenExpirationInMillis(tokenExpirationTime);
 	}
 	
 	@Override
